3e3ba9a102ae76d5f05f74344c675469cccf38df
Frank Eberle <develop@frank-eberle.de>
2025-11-02 21:14:10 +0100

    Fix update issue

--- src/CeTextField.jsx
+++ src/CeTextField.jsx
@@ -1,8 +1,27 @@
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 export default function CeTextField({label, name, defaultValue, required, readOnly}) {
   readOnly = readOnly !== undefined ? readOnly : false;
   required = required !== undefined ? required : false;
+
+  if (readOnly) {
+    return (
+      <TextField
+          size='small'
+          label={label}
+          value={defaultValue}
+          slotProps={{
+              input: {
+                  readOnly: readOnly,
+              },
+              inputLabel: {
+                shrink: true
+              }
+            }}
+      />
+    );
+  }
 
   return (
     <TextField
